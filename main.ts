9f9eeca7e0fd40f46b58e78784fda1284a1ec2f7
apapacharal <97394826+apapacharal@users.noreply.github.com>
2022-01-15 19:55:21 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    for (let n = 0; n <= 4; n++) {
+        for (let index = 0; index <= 4; index++) {
+            basic.pause(500)
+            led.toggle(index, 0)
+        }
+        for (let index = 0; index <= 4; index++) {
+            basic.pause(500)
+            led.toggle(index, 0)
+        }
+        led.toggle(n, 1)
+    }
+})
+basic.forever(function () {
+	
+})
