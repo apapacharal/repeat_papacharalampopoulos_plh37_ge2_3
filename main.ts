1e78c907fef3af2bfb7832af31709f0064ec5743
apapacharal <97394826+apapacharal@users.noreply.github.com>
2022-01-15 19:58:09 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     for (let n = 0; n <= 4; n++) {
         for (let index = 0; index <= 4; index++) {
-            basic.pause(500)
+            basic.pause(1000)
             led.toggle(index, 0)
         }
         for (let index = 0; index <= 4; index++) {
-            basic.pause(500)
+            basic.pause(1000)
             led.toggle(index, 0)
         }
         led.toggle(n, 1)
